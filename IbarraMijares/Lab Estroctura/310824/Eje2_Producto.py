f660f6e74e5bb6cbe284249a7e3dbe1a8d0712bc
DavidIbarra <33142391@uaz.edu.mx>
2024-08-31 12:05:07 -0600

    created

---
+++ IbarraMijares/Lab Estroctura/310824/Eje2_Producto.py
@@ -0,0 +1,7 @@
+class Producto:
+    def __init__(self, nombre, precio, disponibilidad):
+        self.nombre = nombre
+        self.precio = precio
+        self.disponibilidad = disponibilidad
+
+    def aplicar_descuento
