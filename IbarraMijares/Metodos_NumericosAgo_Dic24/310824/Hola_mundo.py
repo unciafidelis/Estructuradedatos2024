0e0fc11a5c5e5c14f6a23e3b3f4d17bdbe436188
DavidIbarra <33142391@uaz.edu.mx>
2024-08-31 19:11:04 -0600

    edit

--- IbarraMijares/Metodos_NumericosAgo_Dic24/310824/Hola_mundo.py
+++ IbarraMijares/Metodos_NumericosAgo_Dic24/310824/Hola_mundo.py
@@ -1,1 +1,18 @@
 print("Hola Mundo")
+
+nombre = input("Dime tu nombre:")
+print("Hola "+nombre)
+
+ano_nacimiento = input("Dime tu año de nacimiento separado por  - cada digito")
+digito = ano_nacimiento.split("-")
+ 
+numeroSuerte = 0
+for digito in digito:
+    print(digito, type(digito))
+    numeroSuerte += int(digito)
+print("Tu numero de la suerte es: " , suma)
+
+if numeroSuerte % 2 == 0:
+    print("Tu numero de la suerte es par")
+else:
+    print("Tu numero de la suerte es impar")
