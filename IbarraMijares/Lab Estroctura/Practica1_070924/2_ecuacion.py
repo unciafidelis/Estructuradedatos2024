d3df39a831359a8664f408cb25711fcda77e7d7b
davidibarra <33142391@uaz.edu.mx>
2024-09-07 17:23:49 -0600

    edit

--- IbarraMijares/Lab Estroctura/Practica1_070924/2_ecuacion.py
+++ IbarraMijares/Lab Estroctura/Practica1_070924/2_ecuacion.py
@@ -1,11 +1,20 @@
 class Ecuacion:
-    def __init__(self,x , y):
-        self.x = x
-        self.y = y
-    def calcuar_ecuacion(self):
-        return 3*x**2 + 7*x - 15
+    def __init__(self):
+        self.x = self.x = float(input("Introduce numero para dar  valor a X: "))   
 
+    def calcular_y(self):
+        return 3 * self.x ** 2 + 7 * self.x - 15
 
-    def imprimir_ecuacion(self):
-        y = self.calcuar_ecuacion()
-        print(f"El valor de Y para  X = {self.x} es : {y}")
+    def imprimir_y(self):
+        print(f"El numero ingresado para X es {self.x} el resultado de Y es: {self.calcular_y()}")
+
+    def modificar_x(self, nuevo_x):
+        self.x = nuevo_x
+        print(f"El numero ingresado para cambiar el valor de  X ha sido modificado a: {self.x}")
+
+ecuacion = Ecuacion()
+ecuacion.imprimir_y()  
+
+nuevo_x = float(input("Introduce un nuevo numero para dar valor a X: "))
+ecuacion.modificar_x(nuevo_x)
+ecuacion.imprimir_y()
