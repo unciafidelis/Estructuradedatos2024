26b1b80e962bc34b12459ab7cb10899a655ad81e
davidibarra <33142391@uaz.edu.mx>
2024-09-07 16:07:23 -0600

    edit

--- IbarraMijares/Lab Estroctura/Practica1_070924/1_velocidaddelaluz.py
+++ IbarraMijares/Lab Estroctura/Practica1_070924/1_velocidaddelaluz.py
@@ -1,14 +1,25 @@
-import abc
+
 from abc import ABC, abstractmethod
 
-class Velocidaddelaluz(ABC):
-    def __init__(self,segundos, distancia):
-        self.segundos = segundos
-        self.distancia = distancia
-
-
-
-
-
+class calculardistancia(ABC):
+    velocidadluz = 300000
+    def tiempoensegundos(self):
+        pass
 
     def imprimir_distancia_segundos(self):
+        pass
+
+class calculardistanciaensegundos(calculardistancia):
+    def __init__(self,tiempoensegundos):
+        self.tiempoensegundos = tiempoensegundos
+    
+    def ingreso_tiempoensegundos(self):
+        return self.tiempoensegundos
+
+    def imprimir_distancia_segundos(self):
+        distancia = self.velocidadluz * self.tiempoensegundos()
+        print(f"La distancia que recorre en {self.ingreso_tiempoensegundos()} segundos en : {distancia} kilometros")
+
+tiempo = float(input("Ingresar tiempo en segundos"))
+calculo = calculardistanciaensegundos(tiempo)
+calculo.imprimir_distancia_segundos() 
