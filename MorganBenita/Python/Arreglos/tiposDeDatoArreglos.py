b8ad49afbbc912c57b7e9e6efa967f47dc0f4797
Alejandro Morgan <MY_NAME@example.com>
2024-09-07 12:14:20 -0600

    create tiposDeDatoArreglos.py

---
+++ MorganBenita/Python/Arreglos/tiposDeDatoArreglos.py
@@ -0,0 +1,11 @@
+import numpy as np
+arreglo = []
+arreglo.append(3)
+arreglo.append("x")
+arreglo.append(3.14)
+
+a = np.array([True])
+a = np.append(a, [True, False, True])
+print(a)
+
+# String > Float > Int > Boolean
