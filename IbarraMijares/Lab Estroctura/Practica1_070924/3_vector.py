58d0004b452ccaaceb9439023ae931759c7341f1
davidibarra <33142391@uaz.edu.mx>
2024-09-07 17:38:51 -0600

    edit

--- IbarraMijares/Lab Estroctura/Practica1_070924/3_vector.py
+++ IbarraMijares/Lab Estroctura/Practica1_070924/3_vector.py
@@ -1,4 +1,36 @@
 class Vector:
-    def __init__(self, a, b):
-        self.a = a
-        self.b= b
+    def __init__(self):
+        self.x = float(input("Introduce el valor de X: "))  
+        self.y = float(input("Introduce el valor de Y: ")) 
+
+    
+    def __mul__(self, escalar):
+        return VectorResultado(self.x * escalar, self.y * escalar)
+
+    
+    def __rmul__(self, escalar):
+        return self.__mul__(escalar)
+
+    
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+
+class VectorResultado:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+
+v = Vector() 
+print("El Vector ingresado por el usuario es:", v)
+
+
+escalar = float(input("Introduce el escalar para multiplicar el vector: "))
+
+v_escalar = v * escalar
+print("Resultado de la mutlipicacion:", v_escalar)
