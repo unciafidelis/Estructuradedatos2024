baec47adf81feb5fc38988d20bb3fbdcb5bcbc2a
davidibarra <33142391@uaz.edu.mx>
2024-09-07 18:45:51 -0600

    EDIT

--- IbarraMijares/Lab Estroctura/Practica1_070924/4_libro.py
+++ IbarraMijares/Lab Estroctura/Practica1_070924/4_libro.py
@@ -2,3 +2,19 @@ class Libro:
     def __init__(self, titulo, autor):
         self.titulo = titulo
         self.autor = autor
+
+    def __eq__(self, otro_libro):
+        return self.titulo == otro_libro.titulo
+
+    def __lt__(self, otro_libro):
+        return self.titulo < otro_libro.titulo
+
+titulo1 = input("Introduce el título del primer libro: ")
+autor1 = input("Introduce el autor del primer libro: ")
+libro1 = Libro(titulo1, autor1)
+
+titulo2 = input("\nIntroduce el título del segundo libro: ")
+autor2 = input("Introduce el autor del segundo libro: ")
+libro2 = Libro(titulo2, autor2)
+
+print(f"\n¿Los títulos son iguales? {libro1 == libro2}")
--- IbarraMijares/Lab Estroctura/Practica1_070924/5_calculadora_area.py
+++ IbarraMijares/Lab Estroctura/Practica1_070924/5_calculadora_area.py
@@ -1,26 +1,32 @@
-import math
 class Circulo:
     def __init__(self, radio):
         self.radio = radio
 
-    def area_circulo(self):
-        return (math.pi * (radio**2))
+    def area(self):
+        return 3.14 * self.radio ** 2
 
 class Rectangulo:
-    def __init__(self, ancho, largo):
-        self.ancho = ancho
+    def __init__(self, largo, ancho):
         self.largo = largo
+        self.ancho = ancho
 
-    def area_rectangulo(self):
-        return largo * ancho
+    def area(self):
+        return self.largo * self.ancho
 
-Circulo = Circulo(5)
-Rectangulo = Rectangulo(4, 6)
-
-
-
-print("Area del Circulo", Circulo.calcular_area())
-print("Area del Rectangulo",Rectangulo.calcular_area())
+class Calculadora_Area:
+    def calcular_area(self, figura):
+        return figura.area()
 
 
+radio = float(input("Introduce el radio del círculo: "))
+circulo = Circulo(radio)
 
+largo = float(input("Introduce el largo del rectángulo: "))
+ancho = float(input("Introduce el ancho del rectángulo: "))
+rectangulo = Rectangulo(largo, ancho)
+
+calculadora = Calculadora_Area()
+
+
+print("Área del círculo:", calculadora.calcular_area(circulo))
+print("Área del rectángulo:", calculadora.calcular_area(rectangulo))
