786983d4da5d0b714719ce6b3062bce2e2f00aa0
Alejandro Morgan <MY_NAME@example.com>
2024-10-19 12:22:55 -0600

    Create hanoiIter.py no esta listo

---
+++ MorganBenita/Python/Recursividad/hanoiIter.py
@@ -0,0 +1,9 @@
+def hanoiIter(N,ORIGEN,DESTINO,AUXILIAR):
+    TOPE = 0
+    BAND = False
+    PILAN = 
+    while N > 0 and BAND == False:
+        TOPE+=1
+        PILAN[TOPE] = N
+
+# MUCHO ROLLO POR AHORA
