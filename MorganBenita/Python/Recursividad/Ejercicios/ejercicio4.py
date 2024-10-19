bd6e0718c2e1bc9512cd4568deac55375195c063
Alejandro Morgan <MY_NAME@example.com>
2024-10-19 13:12:36 -0600

    Create ejercicio4.py

---
+++ MorganBenita/Python/Recursividad/Ejercicios/ejercicio4.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def invertirArregloIter(arr):
+    inicio = 0
+    fin = len(arr)-1
+    while inicio < fin:
+        arr[inicio],arr[fin] = arr[fin], arr[inicio]
+        inicio+=1
+        fin-=1
+    return arr
+
+A = np.array([1,2,3,4,5])
+print(f"Arreglo original {A}")
+print(f"Arreglo invertido {invertirArregloIter(A)}")
+
